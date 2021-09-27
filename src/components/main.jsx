fa0e1079a58a02d3cdf31f66078ea85ffc1967eb
VallyPrasunaBhamidipati <86006241+VallyPrasunaBhamidipati@users.noreply.github.com>
2021-09-27 14:49:24 +0530

    assets

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,6 +2,9 @@ import React, {Component} from "react";
 import {menuActions} from "../menus";
 import Weather from "weather-view";
 import {Helmet} from "react-helmet";
+import axios from 'axios';
+const jwt = require("jsonwebtoken");
+//import qMessenger from "../../assets/js/qmessenger";
  
 export default class Main extends Component {
     constructor(props) {
@@ -11,6 +14,7 @@ export default class Main extends Component {
          * This component will render based on the values of `this.state.data`.
          * This function will set `this.state.data` using the RootEntity's AppData.
          */
+        this.test2 = this.test2.bind(this)
         this.refreshData_ = () => {
             const {rootRecord, menu} = this.props;
             const data = rootRecord.getData();
@@ -22,9 +26,6 @@ export default class Main extends Component {
         this.setupMenuActions_(rootRecord);
         const data = rootRecord.getData();
         this.state = {data, location: "New York"};
-        console.log("HAndle Load1")
-        this.handleLoad = this.handleLoad.bind(this);
-        console.log("HAndle Load2")
     }
     setupMenuActions_(rootRecord) {
         menuActions.toggleHighlight = () =>
@@ -37,46 +38,77 @@ export default class Main extends Component {
         const {rootRecord} = this.props;
         rootRecord.listen(this.refreshData_);
         this.refreshData_();
-        console.log("*&&&&*", window)
-        window.addEventListener('load', this.handleLoad);
-        console.log("*&&&&*2", window)
-    }
-    handleLoad() {
-        console.log("**************")
-        console.log(window.qMessenger);
-        Promise.resolve({"error":false,"messages":{"alert_message":{"body":"<p>Here are the updates for Discovery App</p>","card_icon":null,"message_type":"Alert","prompt_button_label":"","theme":"Info","title":"Test Title","uuid":"b864325b-2da8-ad5d-96a4-5927eeb241b2","like_token":"eyJhbGciOiJIUzI1NiJ9.eyJsaWtlIjp0cnVlLCJmZWRlcmF0aW9uX2lkIjoidi5iaGFtaWRpcGF0aUBzYWxlc2ZvcmNlLmNvbSIsInVzZXJfaWQiOjksIm1lc3NhZ2VfaWQiOjQ5LCJhcHBfaWQiOiJhZjRkZjM3MC1kZmYyLTQ4NDAtODY2YS0wMjEzYWNjZDA4NjRfc3RhZ2luZyIsIm9yZ19pZCI6Im51bGwifQ.5UkNEcOcmuCrZCpZv2n3NWfVw5qvzb3wkieD008yAZI","dislike_token":"eyJhbGciOiJIUzI1NiJ9.eyJsaWtlIjpmYWxzZSwiZmVkZXJhdGlvbl9pZCI6InYuYmhhbWlkaXBhdGlAc2FsZXNmb3JjZS5jb20iLCJ1c2VyX2lkIjo5LCJtZXNzYWdlX2lkIjo0OSwiYXBwX2lkIjoiYWY0ZGYzNzAtZGZmMi00ODQwLTg2NmEtMDIxM2FjY2QwODY0X3N0YWdpbmciLCJvcmdfaWQiOiJudWxsIn0.1Qk5frHoh9lEeZNrCx-ETNiR9xuYabk7agVVaobejdU","persistent":false},"card_message":null,"prompt_message":null}})
-        .then(function(res) {
-            console.log('value: ' + res);
-            if (!res.error) {
-            // @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'mixp... Remove this comment to see the full error message
-               window.qMessenger.init({
-                  messages: res.messages, // required
-                  symbolsURL: 'https://sfdc-q-static.s3-us-west-2.amazonaws.com/q-messenger/slds/assets/icons/utility-sprite/svg/symbols.svg', // required     
-                  voteURL: '/q_messenger_vote', // required
-               });
-               // @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'mixp... Remove this comment to see the full error message
-               window.qMessenger.displayMessages();
-            }
-         })
-         .catch(err => {
-             console.log("Error with qmessenger", err)
-         })
 
+        var userFederationId = "v.bhamidipati@salesforce.com";
+        var userRole = null;
+        var qMessengerUuid = "af4df370-dff2-4840-866a-0213accd0864_staging";
+        var qMessengerSecretKey = "af4df370-dff2-4840-866a-0213accd0864_staging";
+        var qMessengerAlgorith = "HS256";
+        var org_id = null;
+
+        var token = jwt.sign({ 
+            user_federation_id: userFederationId, 
+            user_role: userRole, 
+            app_uuid: qMessengerUuid, 
+            org_id: org_id },
+            qMessengerSecretKey, { algorithm: qMessengerAlgorith }
+        );
+
+        var urlString = "https://q-messenger-staging.qlabs.sfdc.sh/v1/fetch_app_messages" + 
+            "?app_uuid=" + qMessengerUuid + 
+            "&user_federation_id=" + userFederationId + 
+            "&user_role=" + userRole +
+            "&org_id=" + org_id;
+
+        let opts = { headers: { "Content-Type": "application/json", Authorization: "Basic " + token } };
+        axios.get( urlString, opts).then(res => {
+            console.log("$$$",res.data)
+        }).catch(err => {
+            console.log(err)
+        })
+       
+        setTimeout(this.test2,5000);
+
+        const urlString2 = "https://api.publicapis.org/entries";
+        axios.get( urlString2).then(res => {
+            console.log(res.data)
+        }).catch(err => {
+            console.log(err)
+        })
+       
+        // window.qMessenger.init({
+        //     messages: res.messages, // required
+        //     symbolsURL: 'https://sfdc-q-static.s3-us-west-2.amazonaws.com/q-messenger/slds/assets/icons/utility-sprite/svg/symbols.svg', // required     
+        //     voteURL: '/q_messenger_vote', // required
+        // });
+        // window.qMessenger.displayMessages();
+    }
+    test2 = () =>
+    {
+        console.log('value: ',window.qMessenger);
+        const res = {"error":false,"messages":{"alert_message":{"body":"<p>Here are the updates for Discovery App</p>","card_icon":null,"message_type":"Alert","prompt_button_label":"","theme":"Info","title":"Test Title","uuid":"b864325b-2da8-ad5d-96a4-5927eeb241b2","like_token":"eyJhbGciOiJIUzI1NiJ9.eyJsaWtlIjp0cnVlLCJmZWRlcmF0aW9uX2lkIjoidi5iaGFtaWRpcGF0aUBzYWxlc2ZvcmNlLmNvbSIsInVzZXJfaWQiOjksIm1lc3NhZ2VfaWQiOjQ5LCJhcHBfaWQiOiJhZjRkZjM3MC1kZmYyLTQ4NDAtODY2YS0wMjEzYWNjZDA4NjRfc3RhZ2luZyIsIm9yZ19pZCI6Im51bGwifQ.5UkNEcOcmuCrZCpZv2n3NWfVw5qvzb3wkieD008yAZI","dislike_token":"eyJhbGciOiJIUzI1NiJ9.eyJsaWtlIjpmYWxzZSwiZmVkZXJhdGlvbl9pZCI6InYuYmhhbWlkaXBhdGlAc2FsZXNmb3JjZS5jb20iLCJ1c2VyX2lkIjo5LCJtZXNzYWdlX2lkIjo0OSwiYXBwX2lkIjoiYWY0ZGYzNzAtZGZmMi00ODQwLTg2NmEtMDIxM2FjY2QwODY0X3N0YWdpbmciLCJvcmdfaWQiOiJudWxsIn0.1Qk5frHoh9lEeZNrCx-ETNiR9xuYabk7agVVaobejdU","persistent":false},"card_message":null,"prompt_message":null}};
+        window.qMessenger.init({
+            messages: res.messages, // required
+            symbolsURL: 'https://sfdc-q-static.s3-us-west-2.amazonaws.com/q-messenger/slds/assets/icons/utility-sprite/svg/symbols.svg', // required     
+            voteURL: '/q_messenger_vote', // required
+        });
+        window.qMessenger.displayMessages();
     }
     componentWillUnmount() {
         const {rootRecord} = this.props;
         rootRecord.unlisten(this.refreshData_);
-        window.removeEventListener('load', this.handleLoad) 
+        // <Helmet>
+        //     <meta charSet="utf-8" />
+        //     <script src="https://sfdc-q-static.s3.us-west-2.amazonaws.com/q-messenger/javascript/qmessenger.js"></script>
+        // </Helmet>
+    
     }
     render() {
     const { data, location } = this.state;
     return (
     <div className={"root"}>
         Hello5
-        <Helmet>
-            <meta charSet="utf-8" />
-            <script src="https://sfdc-q-static.s3.us-west-2.amazonaws.com/q-messenger/javascript/qmessenger.js"></script>
-        </Helmet>
+        
         <input
         type="text"
         value={location}
