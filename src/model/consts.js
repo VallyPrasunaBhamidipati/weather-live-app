8992125c6e5db8aafba4c5387788a69f4bb16bd3
VallyPrasunaBhamidipati <86006241+VallyPrasunaBhamidipati@users.noreply.github.com>
2021-10-13 11:41:22 +0530

    Update secrets in consts file

--- src/model/consts.js
+++ src/model/consts.js
@@ -1,7 +1,7 @@
 export const REACT_APP_NODE_ENV="development";
 export const REACT_APP_Q_MESSENGER_URL="https://q-messenger-staging.qlabs.sfdc.sh/v1/fetch_app_messages";
 export const Q_MESSENGER_VOTE_URL="https://q-messenger.qlabs.sfdc.sh/v1/upsert_vote";
-export const Q_MESSENGER_APP_UUID="af4df370-dff2-4840-866a-0213accd0864_staging";
-export const Q_MESSENGER_SECRET_KEY="mW5QwbgM7cn27BWzwhb8Vf7bvEQyUKDr3pejexZk9DSHRmdsCLtchd7U99DQWR7a";
+export const Q_MESSENGER_APP_UUID="QUUID";
+export const Q_MESSENGER_SECRET_KEY="QSECRET";
 export const Q_MESSENGER_ALGO="HS256";
 export const Q_MESSENGER_FEDERATION_ID="v.bhamidipati@salesforce.com";
